cc7e8b9048a49cd6f695a36539c058fc2ecdd308
Miha0809 <miha080104@gmail.com>
2025-02-03 08:37:06 +0100

    Init home page. Header and declarate module files.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Home } from "./pages/Home";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <p>Hello, World</p>
+    <div className="container">
+      <Home />
     </div>
   );
 };
